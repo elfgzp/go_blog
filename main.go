f28a0a757b4971988206bde561f672b28dcda275
Gzp_ <741424975@qq.com>
2019-01-06 23:26:27 +0800

    [ADD] add PopulateTemplates func

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // User struct
@@ -23,6 +25,54 @@ type IndexViewModel struct {
 	Posts []Post
 }
 
+// PopulateTemplates func
+// Create map template name to template.Template
+func PopulateTemplates() map[string]*template.Template {
+	const basePath = "templates"
+	result := make(map[string]*template.Template)
+	layout := template.Must(template.ParseFiles(basePath + "/_base.html"))
+	dir, err := os.Open(basePath + "/content")
+
+	if err != nil {
+		panic("Failed to open tempalte blocks directory: " + err.Error())
+	}
+
+	fis, err := dir.Readdir(-1)
+
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+
+	for _, fi := range fis {
+		func() {
+			f, err := os.Open(basePath + "/content/" + fi.Name())
+
+			if err != nil {
+				panic("Failed to open template '" + fi.Name() + "'")
+			}
+
+			defer f.Close()
+
+			content, err := ioutil.ReadAll(f)
+
+			if err != nil {
+				panic("Failed to read content from file '" + fi.Name() + "'")
+			}
+
+			tmpl := template.Must(layout.Clone())
+			_, err = tmpl.Parse(string(content))
+
+			if err != nil {
+				panic("Failed to parse contents of '" + fi.Name() + "' as template")
+			}
+
+			result[fi.Name()] = tmpl
+		}()
+	}
+
+	return result
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		u1 := User{Username: "elfgzp"}
@@ -33,11 +83,10 @@ func main() {
 			{User: u2, Body: "The Avengers movie was so cool!"},
 		}
 
-		v := IndexViewModel{Title: "Homepage", User:u1, Posts:posts}
+		v := IndexViewModel{Title: "Homepage", User: u1, Posts: posts}
 
-		tpl, _ := template.ParseFiles("templates/index.html")
-
-		tpl.Execute(w, &v)
+		templates := PopulateTemplates()
+		templates["index.html"].Execute(w, &v)
 	})
 	http.ListenAndServe(":8888", nil)
 }
